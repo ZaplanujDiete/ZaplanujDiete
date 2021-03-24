60c614f5dd6043671ffbae988ef207be512de8eb
Kiczu <adrian.mamyto@gmail.com>
2021-03-24 16:45:43 +0100

    Fixed linking shop

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+import { checkName } from "./saveName.js";
+if (!checkName) window.location.replace("./app.html");
 const hamburgerIcon = document.querySelector(".hamburger__menu");
 const columnMenu = document.querySelector(".column__menu");
 
